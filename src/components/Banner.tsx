c474244a4f3bb1e6d1b61acc92831fb8e1142b54
kaikaci12 <somewonesomewone@gmail.com>
2024-06-05 16:54:27 +0400

    add slider arrows functionality

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
-
+import { useState, useEffect } from "react";
 const slides = [
   {
     url: "https://img.freepik.com/free-psd/11-11-flash-sale-youtube-banner-template_23-2150826890.jpg?w=1380&t=st=1717590457~exp=1717591057~hmac=a037c99a19ffc7d0f4d26ff7e8015579d06645269ffbdfa17e3cea0833cc2ad1",
@@ -14,16 +14,33 @@ const slides = [
 ];
 
 export default function Banner() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  function prevSlide() {
+    const isFirst = currentIndex === 0;
+    const newIndex = isFirst ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+  const nextSlide = () => {
+    const isLast = currentIndex === slides.length - 1;
+    const newIndex = isLast ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
   return (
     <div className="max-w-[1400px] h-[780px] w-full  m-auto py-16 relative group">
       <div
-        style={{ backgroundImage: `url(${slides[1].url})` }}
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="h-full w-full rounded-2xl  bg-center  bg-contain  duration-500"
       ></div>
-      <div className="hidden group-hover:block absolute top-[50%] translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+      <div
+        onClick={prevSlide}
+        className="hidden group-hover:block absolute top-[50%] translate-x-0 traslate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer "
+      >
         <BsChevronCompactLeft size={30} />
       </div>
-      <div className=" hidden group-hover:block absolute top-[50%] translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ">
+      <div
+        onClick={nextSlide}
+        className=" hidden group-hover:block absolute top-[50%] translate-x-0 traslate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer "
+      >
         <BsChevronCompactRight size={30} />
       </div>
     </div>
